b21dba5440d13070013d19f464603ab8385c7885
DannySF01 <danielsilva01-@hotmail.com>
2026-04-15 09:03:38 +0100

    refactor: improve styles

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,94 +1,89 @@
+import { memo } from "react";
 import { useGameContext } from "../../contexts/GameContext";
 import "./Board.css";
+
+interface LetterProps {
+  letter: string;
+  state: string;
+  isRevealed: boolean;
+  isJustRevealed: boolean;
+  letterPosition: number;
+  id: string;
+}
+
+const Letter = memo(
+  ({
+    letter,
+    state,
+    isRevealed,
+    isJustRevealed,
+    letterPosition,
+    id,
+  }: LetterProps) => {
+    return (
+      <div
+        className={`letter ${state} ${isRevealed ? "reveal" : ""}`}
+        id={id}
+        style={{
+          animationDelay: isJustRevealed ? `${letterPosition * 150}ms` : "0ms",
+        }}
+      >
+        {letter}
+      </div>
+    );
+  },
+);
 
 export function Board() {
   const { currentRow, board, word } = useGameContext();
 
-  function Letter({
-    letterPosition,
-    attemtPosition,
-  }: {
-    letterPosition: number;
-    attemtPosition: number;
-  }) {
-    const STATES = {
-      CORRECT: "correct",
-      PRESENT: "present",
-      ABSENT: "absent",
-      ACTIVE: "active",
-    };
-
-    const letter = board[attemtPosition][letterPosition];
-
-    const correct = word[letterPosition] === letter;
-
-    const present = !correct && letter !== "" && word.includes(letter);
-
-    /* const absent =
-      !correct && !present && value !== "" && !word.includes(value); */
-
-    const id = attemtPosition.toString() + letterPosition.toString();
-
-    const state =
-      (currentRow > attemtPosition &&
-        (correct
-          ? STATES.CORRECT
-          : present
-            ? STATES.PRESENT
-            : STATES.ABSENT)) ||
-      (letter === "" ? "" : STATES.ACTIVE);
-
-    return (
-      <div className={"letter " + state} id={id}>
-        {letter}
-      </div>
-    );
-  }
+  const STATES = {
+    CORRECT: "correct",
+    PRESENT: "present",
+    ABSENT: "absent",
+    ACTIVE: "active",
+  };
 
   return (
     <div className="Board">
-      <div className="row" id="0">
-        <Letter letterPosition={0} attemtPosition={0} />
-        <Letter letterPosition={1} attemtPosition={0} />
-        <Letter letterPosition={2} attemtPosition={0} />
-        <Letter letterPosition={3} attemtPosition={0} />
-        <Letter letterPosition={4} attemtPosition={0} />
-      </div>
-      <div className="row" id="1">
-        <Letter letterPosition={0} attemtPosition={1} />
-        <Letter letterPosition={1} attemtPosition={1} />
-        <Letter letterPosition={2} attemtPosition={1} />
-        <Letter letterPosition={3} attemtPosition={1} />
-        <Letter letterPosition={4} attemtPosition={1} />
-      </div>
-      <div className="row" id="2">
-        <Letter letterPosition={0} attemtPosition={2} />
-        <Letter letterPosition={1} attemtPosition={2} />
-        <Letter letterPosition={2} attemtPosition={2} />
-        <Letter letterPosition={3} attemtPosition={2} />
-        <Letter letterPosition={4} attemtPosition={2} />
-      </div>
-      <div className="row" id="3">
-        <Letter letterPosition={0} attemtPosition={3} />
-        <Letter letterPosition={1} attemtPosition={3} />
-        <Letter letterPosition={2} attemtPosition={3} />
-        <Letter letterPosition={3} attemtPosition={3} />
-        <Letter letterPosition={4} attemtPosition={3} />
-      </div>
-      <div className="row" id="4">
-        <Letter letterPosition={0} attemtPosition={4} />
-        <Letter letterPosition={1} attemtPosition={4} />
-        <Letter letterPosition={2} attemtPosition={4} />
-        <Letter letterPosition={3} attemtPosition={4} />
-        <Letter letterPosition={4} attemtPosition={4} />
-      </div>
-      <div className="row" id="5">
-        <Letter letterPosition={0} attemtPosition={5} />
-        <Letter letterPosition={1} attemtPosition={5} />
-        <Letter letterPosition={2} attemtPosition={5} />
-        <Letter letterPosition={3} attemtPosition={5} />
-        <Letter letterPosition={4} attemtPosition={5} />
-      </div>
+      {board.map((row, attemtPosition) => (
+        <div
+          className="row"
+          key={attemtPosition}
+          id={attemtPosition.toString()}
+        >
+          {row.map((_, letterPosition) => {
+            const letter = board[attemtPosition][letterPosition];
+            const isRevealed = currentRow > attemtPosition;
+            const isJustRevealed = currentRow === attemtPosition + 1;
+
+            const correct = word[letterPosition] === letter;
+            const present = !correct && letter !== "" && word.includes(letter);
+
+            const state = isRevealed
+              ? correct
+                ? STATES.CORRECT
+                : present
+                  ? STATES.PRESENT
+                  : STATES.ABSENT
+              : letter === ""
+                ? ""
+                : STATES.ACTIVE;
+
+            return (
+              <Letter
+                key={`${attemtPosition}-${letterPosition}`}
+                letter={letter}
+                state={state}
+                isRevealed={isRevealed}
+                isJustRevealed={isJustRevealed}
+                letterPosition={letterPosition}
+                id={attemtPosition.toString() + letterPosition.toString()}
+              />
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 }
